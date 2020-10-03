ec4f9b20efb6b8d4dcf8231dc8d91298d3b47020
Brahim kanouche <bkano020@uottawa.ca>
2020-10-03 17:51:03 -0400

    combined features:login signup rooms and theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: '',
     redirectTo: 'login',
     pathMatch: 'full'
   },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginModule),data: { preload: true }
+  },
+   {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule),data: { preload: true }
+  },
+  {
+    path: 'rooms',
+    loadChildren: () => import('./pages/rooms/rooms.module').then( m => m.RoomsPageModule),data: { preload: true }
   },
   {
     path: 'themes',
